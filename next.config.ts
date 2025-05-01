4199c6f3dca135853ca422366b012a532b8c6129
Melrain <yezyd92@icloud.com>
2025-05-01 17:46:11 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isDev = process.env.NODE_ENV === 'development';
+
+const nextConfig = {
+  reactStrictMode: true,
+  // 不要写 i18n，这会报 warning
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: !isDev, // ⛔ 不注册 service worker 在 dev 模式
+  skipWaiting: true,
+  disable: isDev, // ✅ 禁用 PWA 功能
+})(nextConfig);
