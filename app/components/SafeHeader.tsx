ff261cba1e775966f714b76bc046b9fef88adbfe
Melrain <yezyd92@icloud.com>
2025-05-02 10:08:56 +0800

    1

--- app/components/SafeHeader.tsx
+++ app/components/SafeHeader.tsx
@@ -4,5 +4,9 @@ import { useAppEnv } from "../store/useAppEnv";
 
 export function SafeHeader() {
   const { isTelegram } = useAppEnv();
-  return <header className={`${isTelegram ? "pt-24" : ""}`}>Header</header>;
+  return (
+    <header className={`${isTelegram ? "pt-28" : "pt-6"} `}>
+      {isTelegram ? "这是telegram环境" : "非telegram 环境"}
+    </header>
+  );
 }
